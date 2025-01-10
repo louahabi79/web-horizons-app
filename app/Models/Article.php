5947b879ce8715c3f95c29b35f6a1ed2d1683699
hamzaaityoussef <153993959+hamzaaityoussef@users.noreply.github.com>
2025-01-10 17:02:52 +0100

    update models

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -14,6 +15,9 @@ class Article extends Model
         'statut',
         'date_proposition',
         'date_publication',
+        'image_couverture',
+        'temps_lecture',
+        'vues',
         'theme_id',
         'user_id'
     ];
@@ -46,5 +50,23 @@ class Article extends Model
     public function navigationHistory(): HasMany
     {
         return $this->hasMany(NavigationHistory::class);
+    }
+
+    // Article's tags
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // Article's numero
+    public function numero(): BelongsTo
+    {
+        return $this->belongsTo(Numero::class);
+    }
+
+    // Méthode pour calculer la note moyenne
+    public function averageRating()
+    {
+        return $this->notes()->avg('note');
     }
 }
